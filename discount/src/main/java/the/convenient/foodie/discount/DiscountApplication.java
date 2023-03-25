cf35e005cda1f2c2a9b9be8f3798ac2358f16f16
Irvin Catic <icatic1@etf.unsa.ba>
2023-03-25 20:01:13 +0100

    Add Controller, Entity, Repository for Coupon

--- discount/src/main/java/the/convenient/foodie/discount/DiscountApplication.java
+++ discount/src/main/java/the/convenient/foodie/discount/DiscountApplication.java
@@ -1,13 +1,34 @@
 package the.convenient.foodie.discount;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import the.convenient.foodie.discount.dao.CouponRepository;
+import the.convenient.foodie.discount.entity.Coupon;
 
+@EnableJpaRepositories("the.convenient.foodie.discount.dao")
+@EntityScan(basePackages = "the.convenient.foodie.discount.entity")
 @SpringBootApplication
-public class DiscountApplication {
+public class DiscountApplication implements CommandLineRunner {
+
+	@Autowired
+	CouponRepository couponRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscountApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception
+	{
+		dumpData();
+	}
+
+	private void dumpData() {
+		Coupon c = new Coupon("kod1",3,2,10,"uuidkod1");
+		couponRepository.save(c);
 	}
 
 }
---
+++ discount/src/main/java/the/convenient/foodie/discount/controller/CouponController.java
@@ -0,0 +1,29 @@
+package the.convenient.foodie.discount.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import the.convenient.foodie.discount.dao.CouponRepository;
+import the.convenient.foodie.discount.entity.Coupon;
+
+@Controller // This means that this class is a Controller
+@RequestMapping(path="/coupon") // This means URL's start with /demo (after Application path)
+public class CouponController {
+    private CouponRepository couponRepository;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody
+    String addNewCoupon (@RequestParam String code, @RequestParam Integer quantity, @RequestParam Integer restaurant_id,@RequestParam Integer discount_percentage,@RequestParam String guid) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        Coupon n = new Coupon(code,quantity,restaurant_id,discount_percentage,guid);
+        couponRepository.save(n);
+        return "Added coupon";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Coupon> getAllCoupons() {
+        // This returns a JSON or XML with the users
+        return couponRepository.findAll();
+    }
+}
