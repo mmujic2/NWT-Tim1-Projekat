443580bf55afb25ae8ee19bb7b02ea83374d4183
Samra Behic <62614263+Samra17@users.noreply.github.com>
2023-03-29 13:15:07 +0200

    change error handling response

--- menu/src/main/java/the/convenient/foodie/menu/exception/RestExceptionHandler.java
+++ menu/src/main/java/the/convenient/foodie/menu/exception/RestExceptionHandler.java
@@ -14,10 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -45,28 +42,29 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    private ResponseEntity<String> handleEntityNotFound(EntityNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    private ResponseEntity<Map<String, List<String>>> handleEntityNotFound(EntityNotFoundException ex){
+        List<String> errors = new ArrayList<>();
+        errors.add(ex.getMessage().toString());
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    private ResponseEntity<String> handleEmptyResultDataAccessNotFound(EmptyResultDataAccessException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    private ResponseEntity<Map<String, List<String>>> handleEmptyResultDataAccessNotFound(EmptyResultDataAccessException ex){
+        List<String> errors = new ArrayList<>();
+        errors.add(ex.getMessage().toString());
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity handle(ConstraintViolationException constraintViolationException) {
-        Set<ConstraintViolation<?>> violations = constraintViolationException.getConstraintViolations();
-        String errorMessage = "";
-        if (!violations.isEmpty()) {
-            StringBuilder builder = new StringBuilder();
-            violations.forEach(violation -> builder.append("\n" + violation.getMessage()));
-            errorMessage = builder.toString();
-        } else {
-            errorMessage = "ConstraintViolationException occured.";
-        }
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, List<String>>> handleUpdateValidationErrors(ConstraintViolationException ex) {
+        List<String> errors = ex.getConstraintViolations().stream().map(ConstraintViolation::getMessage).toList();
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
-
+    private Map<String, List<String>> messageToMap(String message) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        List<String> messageList = new ArrayList<>(); messageList.add(message);
+        errorResponse.put("errors", messageList);
+        return errorResponse;
+    }
 }
