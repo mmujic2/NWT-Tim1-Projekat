b2cce935f2904e5101da5814fec318f0b7a6ae48
mmujic2 <73075272+mmujic2@users.noreply.github.com>
2023-03-29 17:27:58 +0200

    Add config server

---
+++ foodieConfigServer/src/test/java/fooide/config/server/foodieConfigServer/FoodieConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fooide.config.server.foodieConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
