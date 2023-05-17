fd44e954eecfe46820b0790637c0b80ec37f476c
ahadzibajr1 <ahadzibajr1@etf.unsa.ba>
2023-05-17 12:54:52 +0200

    Add axios

--- frontend/src/shared/Login.js
+++ frontend/src/shared/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Row from 'react-bootstrap/Row';
 import {
   MDBBtn,
   MDBContainer,
@@ -17,11 +18,19 @@ import logo from "./logo.png";
 
 function Login() {
   return (
-    <MDBContainer fluid className="p-3 my-5 d-flex flex-column w-50">
-      <MDBContainer className="p-3 text-white ">
+    <>
+    <Row>
+    <img src={logo} alt="..." />
+        <h2 className="fw-bold mb-2 text-center" >The Convenient Foodie</h2>
+    </Row>
+    <div className="row">
         <img src={logo} alt="..." />
-        <h2 className="fw-bold mb-2 text-center">The Convenient Foodie</h2>
-      </MDBContainer>
+        <h2 className="fw-bold mb-2 text-center" >The Convenient Foodie</h2>
+      </div>
+    <MDBContainer fluid className="p-3 my-5 d-flex flex-column w-50">
+
+      
+
       <MDBRow className="d-flex justify-content-center align-items-center h-100">
         <MDBCol col="12">
           <MDBCard
@@ -30,7 +39,7 @@ function Login() {
           >
             <MDBCardBody className="p-5 w-100 d-flex flex-column">
               <p className="text-white-50 mb-3">
-                Please enter your login and password!
+                Please enter your username and password!
               </p>
 
               <MDBInput
@@ -59,28 +68,13 @@ function Login() {
 
               <hr className="my-4" />
 
-              <MDBBtn
-                className="mb-2 w-100"
-                size="lg"
-                style={{ backgroundColor: "#dd4b39" }}
-              >
-                <MDBIcon fab icon="google" className="mx-2" />
-                Sign in with google
-              </MDBBtn>
 
-              <MDBBtn
-                className="mb-4 w-100"
-                size="lg"
-                style={{ backgroundColor: "#3b5998" }}
-              >
-                <MDBIcon fab icon="facebook-f" className="mx-2" />
-                Sign in with facebook
-              </MDBBtn>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
       </MDBRow>
     </MDBContainer>
+    </>
   );
 }
 
