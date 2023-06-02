db57cadfdfd456c41f7092722a0618598e189c64
Samra Behic <62614263+Samra17@users.noreply.github.com>
2023-06-02 16:25:45 +0200

    add: nav tabs fot menu overview

--- frontend/src/customer/Restaurant/MenuOverview.js
+++ frontend/src/customer/Restaurant/MenuOverview.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import menuService from "../../service/menu.service";
+import { Tabs, Tab } from "react-bootstrap";
+import "./MenuOverview.css";
 
 function MenuOverview({ restaurantUUID }) {
   const [menus, setMenus] = useState([]);
@@ -16,7 +18,25 @@ function MenuOverview({ restaurantUUID }) {
       });
     }
   }, []);
-  return <div>{JSON.stringify(menus)}</div>;
+  return (
+    <div>
+      <Tabs defaultActiveKey={0} id="my-tabs" style={{ padding: 10 }}>
+        {menus.map((menus, index) => (
+          <Tab
+            eventKey={index}
+            title={<span style={{ color: "black" }}>{menus.name}</span>}
+            key={index}
+          >
+            <div>
+              <h2>{menus.name}</h2>
+              <hr className="tab-separator" />
+              <p>This is the content for {menus.name}</p>
+            </div>
+          </Tab>
+        ))}
+      </Tabs>
+    </div>
+  );
 }
 
 export default MenuOverview;
